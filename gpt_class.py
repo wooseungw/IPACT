b9328e608c87014350925d4d8e69165cd78b69c2
wooseungw <woow070840@gmail.com>
2024-05-26 21:24:36 +0900

    12

--- gpt_class.py
+++ gpt_class.py
@@ -51,9 +51,56 @@ if __name__ == "__main__":
     load_dotenv()
     openai = os.getenv("OPENAI_API_KEY")
     ## Example
-    gpt = GPT(api_key=openai, model="gpt-4o", top_p=1.0)
+    gpt = GPT(api_key=openai, model="gpt-4o-2024-05-13", top_p=1.0)
+    text_prompt = """
+    이 플로우차트를 기반으로 langchain과 rag를 사용해서 코드를 짜줘.
+    import os
+from glob import glob
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain_community.vectorstores import Chroma
+from dotenv import load_dotenv
+load_dotenv()
+
+# Initialize variables
+documents = []
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# Define the directory containing the PDF files
+pdf_directory = './data'
+# pdf를 사용해서 pdf(논문)을 모두 로드
+pdf_files = glob(os.path.join(pdf_directory, '*.pdf'))
+
+# Load all PDF files using PyPDFLoader
+for pdf_file in pdf_files:
+    loader = PyPDFLoader(pdf_file)
+    pdf_documents = loader.load()
+    documents.extend(pdf_documents)
+    
+# 텍스트는 RecursiveCharacterTextSplitter를 사용하여 분할
+chunk_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = chunk_splitter.split_documents(documents)
+
+# embeddings은 OpenAI의 임베딩을 사용
+# vectordb는 chromadb사용함
+embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+vectordb = Chroma.from_documents(documents=chunks, embedding=embeddings)
+retriever = vectordb.as_retriever()
+
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.chains import RetrievalQA
+
+# Initialize the OpenAIEmbeddings and ChatOpenAI objects
+embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+chat_model = ChatOpenAI(api_key=OPENAI_API_KEY, model_name="gpt-4o", temperature=0)
+
+이 코드도 참고해봐.
+
+    """
     img_prompt = []
-    # img_prompt.append('food.png')
+    img_prompt.append('image.png')
     # img_prompt.append('football.png')
-    answer = gpt.generate(text_prompt="이미지의 주소를 가져올때 인터넷의 이미지 주소는 www.을 포함하지?", img_prompt=img_prompt)
+    answer = gpt.generate(text_prompt="", img_prompt=img_prompt)
     print(answer)
